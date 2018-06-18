af14b4fdddbf7fe8a5aa3931b4a751a87d9d04a2
GPC Admin <gpcadmin@GPCs-Mac-mini.local>
2018-06-18 15:56:30 -0400

    Added csvtojson feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PapaParseService } from 'ngx-papaparse';
+import 'rxjs/add/operator/toPromise';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(public http: HttpClient, private papa: PapaParseService) {}
+
+  handleFileSelect(evt) {
+    let file = evt.target.files[0];
+    this.papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: function(csvOrders) {
+        console.log(csvOrders.data);
+        this.http.post(environment.ordersUrl, {'order': csvOrders.data}).toPromise()
+        .then((data)=>console.log(data));
+      }
+    });
+  }
+
+
 }
